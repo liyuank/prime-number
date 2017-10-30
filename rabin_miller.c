b62b3260db28c8b835986264f9574467e796c93e
liyuank <12709879+liyuank@users.noreply.github.com>
2017-10-30 15:31:50 -0700

    Create rabin_miller.c
    Try to implement rabin-miller function in C

---
+++ rabin_miller.c
@@ -0,0 +1,12 @@
+/*
+** function: Rabin-Miller
+** input : n An odd number >= 3
+** output : b Boolean indicating whether n is prime or not
+** assert n >= 3 ^ n mod 2 = 1
+**
+**
+**  Practical Cryptography by Bruce Schneier and Niels Ferguson
+** page 201.
+**
+**
+*/
